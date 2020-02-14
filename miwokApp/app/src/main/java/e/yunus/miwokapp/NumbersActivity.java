afa3a3296b8faa8fb11085b962a5fbd44282ca2e
yunuswidodo <younews372@gmail.com>
2020-02-14 21:03:47 +0700

    Log Tag

--- miwokApp/app/src/main/java/e/yunus/miwokapp/NumbersActivity.java
+++ miwokApp/app/src/main/java/e/yunus/miwokapp/NumbersActivity.java
@@ -3,6 +3,7 @@ package e.yunus.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //create an array of word
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("coba", words[1]);
+
+
     }
 }

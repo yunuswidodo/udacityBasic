c73222c984b30c2c50da41651df3f34e7db50065
yunuswidodo <younews372@gmail.com>
2020-02-23 16:55:03 +0700

    music app make toast if play music is done

--- AndroidBasic/miwokApp/app/src/main/java/e/yunus/miwokapp/NumbersActivity.java
+++ AndroidBasic/miwokApp/app/src/main/java/e/yunus/miwokapp/NumbersActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -87,5 +90,11 @@ public class NumbersActivity extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.list);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(NumbersActivity.this,"toast listener", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
